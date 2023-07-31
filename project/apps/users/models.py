a120544fd7bc5003d8114fea1669cdc99ed2bb96
Federico Tempo <federicotempo98@gmail.com>
2023-07-31 18:33:16 -0300

    model Usuario creado

--- project/apps/users/models.py
+++ project/apps/users/models.py
@@ -1,4 +1,15 @@
 from django.db import models
 from django.forms import CharField
+from django.contrib.auth.models import User
 
 # Usuario
+
+
+class Usuario(models.Model):
+    usuario = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name="usuario")
+    email = models.EmailField(max_length=100)
+    avatar = models.ImageField(upload_to="avatares", blank=True, null=True)
+
+    def __str__(self):
+        return self.usuario.username
