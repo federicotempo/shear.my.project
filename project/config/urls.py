e73bc5060cbdcf49b21512b57c6a880ab8d850d0
Federico Tempo <federicotempo98@gmail.com>
2023-08-01 15:26:08 -0300

    tratando de agregar imagen a proyectos

--- project/config/urls.py
+++ project/config/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,3 +26,6 @@ urlpatterns = [
     path("", include("proyectos.urls")),
     path("", include("autores.urls")),
 ]
+
+if settings.DEBUG == True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
